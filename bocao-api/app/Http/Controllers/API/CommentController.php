9051572298bdb8b905de4098b86b2b3562bc7b20
Anna Mercado <nissathar.15@gmail.com>
2025-05-06 13:13:08 +0200

    Controller and route to be able to edit a comment

--- bocao-api/app/Http/Controllers/API/CommentController.php
+++ bocao-api/app/Http/Controllers/API/CommentController.php
@@ -1,14 +1,25 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use App\Models\Restaurant;
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CommentResource;
+use App\Services\RoleService;
 
 class CommentController extends Controller
 {
+    protected $roleService;
+
+    public function __construct(RoleService $roleService)
+    {
+        $this->roleService = $roleService;
+    }
+
+
     public function create(Request $request, $restaurantId)
     {
         $restaurant = Restaurant::findOrFail($restaurantId);
@@ -26,6 +37,7 @@ class CommentController extends Controller
         return new CommentResource($comment);
     }
 
+ 
     public function read(Request $request, $restaurantId)
     {
         $restaurant = Restaurant::findOrFail($restaurantId);
@@ -33,5 +45,29 @@ class CommentController extends Controller
         $comments = $restaurant->comments()->latest()->get();
 
         return CommentResource::collection($comments);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::findOrFail($id);
+        $user = $request->user();
+
+        if (!$this->roleService->canUpdateComment($user, $comment)) {
+            return response()->json([
+                'message' => 'Unauthorized to update this comment',
+            ], 403);
+        }
+
+        $validated = $request->validate([
+            'content' => ['required', 'string', 'max:255'], 
+        ]);
+
+        $comment->update($validated);
+
+        return response()->json([
+            'message' => 'Comment updated',
+            'data' => $comment,
+        ], 200);
     }
 }
